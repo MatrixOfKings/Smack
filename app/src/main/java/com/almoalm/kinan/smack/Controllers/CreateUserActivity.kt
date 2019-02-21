f4d3dc5e6e68123c26209a759abfb7f0e6672594
kinanA <kinanalmoalm@gmail.com>
2019-02-21 18:00:23 +0200

    MVC

--- app/src/main/java/com/almoalm/kinan/smack/Controllers/CreateUserActivity.kt
+++ app/src/main/java/com/almoalm/kinan/smack/Controllers/CreateUserActivity.kt
@@ -1,9 +1,10 @@
-package com.almoalm.kinan.smack
+package com.almoalm.kinan.smack.Controllers
 
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.almoalm.kinan.smack.R
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
--- app/src/main/java/com/almoalm/kinan/smack/Controllers/LoginActivity.kt
+++ app/src/main/java/com/almoalm/kinan/smack/Controllers/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.almoalm.kinan.smack
+package com.almoalm.kinan.smack.Controllers
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.almoalm.kinan.smack.R
 
 class LoginActivity : AppCompatActivity() {
 
